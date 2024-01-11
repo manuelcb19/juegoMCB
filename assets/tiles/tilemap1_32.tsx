cdef4f9f3b69808c522d96e9566e8bce02f6faab
manuelcb19 <mcb.gitlab@gmail.com>
2024-01-11 09:29:28 +0100

    Se añaden los assets necesarios para el juego, por ahora se va a usar el tiled del profesor, mas adelante se cambiara a uno personalizado

---
+++ assets/tiles/tilemap1_32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tilemap1_32" tilewidth="32" tileheight="32" tilecount="135" columns="9">
+ <image source="../images/tilemap1_32.png" width="288" height="480"/>
+</tileset>
